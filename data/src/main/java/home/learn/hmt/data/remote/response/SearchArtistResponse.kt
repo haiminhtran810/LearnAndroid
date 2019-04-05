9e674fb14eadb9d7bbea2493574028964f76f6bf
haiminhtran810 <haiminhtran810@gmail.com>
2019-04-05 15:01:12 +0700

    [Task] Add Mock API android

--- data/src/main/java/home/learn/hmt/data/remote/response/SearchArtistResponse.kt
+++ data/src/main/java/home/learn/hmt/data/remote/response/SearchArtistResponse.kt
@@ -3,7 +3,7 @@ package home.learn.hmt.data.remote.response
 import com.google.gson.annotations.Expose
 import home.learn.hmt.data.model.ArtistEntity
 
-class SearchArtistResponse(
+data class SearchArtistResponse(
     @Expose
     val artists: List<ArtistEntity>
 )
