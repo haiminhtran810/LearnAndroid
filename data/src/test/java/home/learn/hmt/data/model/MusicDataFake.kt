76c139a130fef6e06a66e1802ea4977e13a02d04
haiminhtran810 <haiminhtran810@gmail.com>
2019-04-08 10:07:36 +0700

    [Task] Add Mock test for Mapper

---
+++ data/src/test/java/home/learn/hmt/data/model/MusicDataFake.kt
@@ -0,0 +1,26 @@
+package home.learn.hmt.data.model
+
+import home.learn.hmt.data.model.ArtistEntity
+import home.learn.hmt.data.model.LyricEntity
+import home.learn.hmt.data.remote.response.SearchArtistResponse
+
+val lyricEntityFake =
+    LyricEntity(
+        "123", 123, "lyrics_body",
+        "script_tracking_url", "pixel_tracking_url",
+        "lyrics_copyright", "updated_time"
+    )
+
+val artistEntityFake = ArtistEntity(
+    1,
+    "artist_name",
+    mutableListOf("A"),
+    "artist_comment",
+    "artist_country",
+    mutableListOf("A"),
+    1,
+    "artist_twitter_url",
+    true,
+    "updated_time"
+)
+val searchArtistResponseFake = SearchArtistResponse(mutableListOf(artistEntityFake))
